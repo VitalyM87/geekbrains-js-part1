1f7841f14bbc4b10b9c8eeca67cb4b380e7addeb
VitalyM87 <makarov.1987@yandex.ru>
2020-10-21 22:37:57 +0300

    reduce

--- hw5.js
+++ hw5.js
@@ -2,12 +2,22 @@
 
 class basketItem{
     constructor(itemName, itemCost, itemWeight) {
-        this.itemName = itemName;
-        this.itemCost = itemCost;
-        this.itemWeight = itemWeight;
+       this.itemName = itemName;
+       this.itemCost = itemCost;
+       this.itemWeight = itemWeight;
     }
 }
 
-const item = new basketItem(`Иммуностимулирующий`, 150, 300,);
 
-console.log(item);
+const item = [
+    new basketItem(`Иммуностимулирующий`, 150, 300,), 
+    new basketItem(`Антистрессовый`, 250, 300,), 
+    new basketItem(`Антиоксидант`, 350, 400,), 
+    new basketItem(`Сердце Байкала`, 400, 450,),
+]
+
+let totalCost = item.reduce(function(accum, item) {
+    return accum + item.itemCost;
+},0);
+
+console.log(totalCost);
